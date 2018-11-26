d7fbe00532cbe07c0bc3a42fea9af8c66f6b0e70
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-26 03:21:07 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,8 +4,7 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  data.each do |seasons, entries|
-    entries.each do |entry|
+  data.values.each do |entry|
       if entry['occupation'] == occupation
         return entry['name']
       end
